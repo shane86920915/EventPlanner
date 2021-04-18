28a6c88af850a16061fb1db683e49c23f090a291
shane86920915 <73564809+shane86920915@users.noreply.github.com>
2021-04-18 19:25:18 -0400

    Added CustomerEventCreate Controller

--- EventPlanner/Controllers/CustomerEventController.cs
+++ EventPlanner/Controllers/CustomerEventController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EventPlanner.Models;
+using EventPlanner.Services;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,28 @@ namespace EventPlanner.Controllers
     public class CustomerEventController : Controller
     {
         // GET: CustomerEvent
-        public ActionResult Index()
+        public ActionResult Create()
         {
+
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CustomerEventCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new CustomerEventService(userId);
+
+            service.CreateCustomerEvent(model);
+
+            return RedirectToAction("Index");
+
+
+        }
+       
+
     }
 }
