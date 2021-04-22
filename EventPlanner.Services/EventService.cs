ac92971fcf762e1b5530ba8311187ce43855ea65
shane86920915 <73564809+shane86920915@users.noreply.github.com>
2021-04-22 17:56:29 -0400

    Added Get Index to EventController

---
+++ EventPlanner.Services/EventService.cs
@@ -0,0 +1,66 @@
+﻿using EventPlanner.Models;
+using EventPlanner.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlanner.Services
+{
+    public class EventService
+    {
+        private readonly Guid _userId;
+
+        public EventService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateEvent(EventCreate model)
+        {
+            var entity
+                = new Event()
+                {
+                    OwnerId = _userId,
+                    EventTitle = model.EventTitle,
+                    Address = model.Address,
+                    City = model.City,
+                    State = model.State,
+                    Price = model.Price,
+                    CreatedUtc = DateTimeOffset.Now,
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Events.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<EventListItem> GetEvent()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var Query
+                    = ctx
+                    .Events
+                    .Where(e => e.OwnerId == _userId)
+                    .Select(
+                        e =>
+                        new EventListItem()
+                        {
+                            EventId = e.EventId,
+                            EventTitle = e.EventTitle,
+                            Address = e.Address,
+                            City = e.City,
+                            State = e.State,
+                            CreatedUtc = DateTimeOffset.Now
+
+                         
+                        }) ;
+                return Query.ToArray();
+            }
+
+        }
+    }
+}
---
+++ EventPlanner/Controllers/EventController.cs
@@ -0,0 +1,28 @@
+﻿using EventPlanner.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventPlanner.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: Event
+        public ActionResult Index()
+        {
+            var service = CreateEventServices();
+            var model = service.GetEvent();
+            return View(model);
+        }
+
+        public EventService CreateEventServices()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            return new EventService(userId);
+        }
+
+    }
+}
